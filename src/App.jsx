68d8281c7b3f73e7ccabe9c427ae655d8aa74777
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2023-03-03 13:40:22 +0000

    adding paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 export default function App(props){
     return (
+        <>
         <h1 className="text-center text-primary">{props.message}</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, vitae omnis repudiandae dignissimos nemo doloribus dolorum exercitationem dolores aspernatur debitis, ducimus quisquam officia facilis explicabo enim, sit eligendi a eius!</p>
+        </>
     )
 }
